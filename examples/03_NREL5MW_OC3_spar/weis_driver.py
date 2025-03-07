541c4893ebd12649208e3e12204ee6733fe87f83
rbehrensdeluna <r.behrensdeluna@tu-berlin.de>
2025-03-07 11:00:22 +0100

    update examples to WEIS 1.5

--- examples/03_NREL5MW_OC3_spar/weis_driver.py
+++ examples/03_NREL5MW_OC3_spar/weis_driver.py
@@ -2,17 +2,10 @@ import os
 from weis import weis_main
 
 ## File management
-<<<<<<< HEAD
 run_dir = os.path.dirname( os.path.realpath(__file__) )
 fname_wt_input = os.path.join(run_dir, "..", "00_setup", "ref_turbines", "nrel5mw-spar_oc3.yaml")
 fname_modeling_options = os.path.join(run_dir, "modeling_options.yaml")
 fname_analysis_options = os.path.join(run_dir, "analysis_options.yaml")
-=======
-run_dir                 = os.path.dirname( os.path.realpath(__file__) )
-fname_wt_input          = run_dir + os.sep + "nrel5mw-spar_oc3.yaml"
-fname_modeling_options  = run_dir + os.sep + 'modeling_options_test.yaml'
-fname_analysis_options  = run_dir + os.sep + 'analysis_options_noopt.yaml'
->>>>>>> test-develop
 
 wt_opt, modeling_options, opt_options = weis_main(fname_wt_input, 
                                                  fname_modeling_options, 
